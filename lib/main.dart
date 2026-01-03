51c2207e0a6a66cf4580fc8fe997b18af7fbcfda
Zzynnyxx <collinsmwenda888@gmail.com>
2026-01-03 22:06:36 +0300

    connecting the demo app to android phone

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,22 @@ void main(){
   runApp(const MyFirstApp());
 }
 
+
 class MyFirstApp extends StatelessWidget {
   const MyFirstApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
        appBar: AppBar(
-        title: const Text('Ummh Development Lab'),
+        title: const Text('Ummh Development  finished Lab'),
         backgroundColor:Colors.blueGrey ,
        ), 
        body: const Center(
         child: Text(
-          'Hello , Flutter!!',
+          'Hello , Flutter  lets get  started right now . But first what is the problem!!',
           style: TextStyle(fontSize: 24),
         ),
        ),
